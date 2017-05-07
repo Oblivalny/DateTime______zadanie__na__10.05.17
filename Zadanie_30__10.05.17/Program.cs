f40bda64fc0a3ec4c5cd169666893c71d71e3c17
днс <@DESKTOP-P6D9CRD>
2017-05-07 19:57:19 +0500

    11

---
+++ Zadanie_30__10.05.17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_30__10._05._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Дан файл f, компоненты которого являются целыми числами. Никакая
+            из компонент файла не равна нулю. Файл f содержит столько же отри-
+            цательных чисел, сколько и положительных. Используя вспомогатель-
+            ный файл h, переписать компоненты файла f в файл g так, чтобы в
+            файле g сначала шли нечетные потом четные числа.*/
+
+
+
+        }
+    }
+}
